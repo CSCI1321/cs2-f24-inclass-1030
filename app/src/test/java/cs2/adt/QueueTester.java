b3e76b1fca0a819c59fce3461ec0b6fa77ec6ff4
Matthew Hibbs <mhibbs@trinity.edu>
2024-11-22 10:31:52 -0600

    queue tester

--- app/src/test/java/cs2/adt/QueueTester.java
+++ app/src/test/java/cs2/adt/QueueTester.java
@@ -4,18 +4,62 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.BeforeEach;
 
-
-public class QueueTester {
+class QueueTester {
   private Queue<Integer> q;
 
   @BeforeEach
   void init() {
-
+    
   }
 
   @Test
-  void testSingle() {
-    
+  void testEnDeOnce() {
+    assertTrue(q.isEmpty());
+    q.enqueue(7);
+    assertFalse(q.isEmpty());
+    assertEquals(q.peek(), 7);
+    assertEquals(q.dequeue(), 7);
+    assertTrue(q.isEmpty());
+  }
+
+  @Test
+  void testEnDeMulti() {
+    assertTrue(q.isEmpty());
+    for(int i=0; i<100; i++) {
+      q.enqueue(i);
+      assertFalse(q.isEmpty());
+      assertEquals(q.peek(), 0);
+    }
+    for(int i=0; i<100; i++) {
+      assertEquals(q.peek(), i);
+      assertEquals(q.dequeue(), i);
+    }
+    assertTrue(q.isEmpty());
+  }
+
+  @Test
+  void testEnDeMultiTwice() {
+    assertTrue(q.isEmpty());
+    for(int i=0; i<100; i++) {
+      q.enqueue(i);
+      assertFalse(q.isEmpty());
+      assertEquals(q.peek(), 0);
+    }
+    for(int i=0; i<100; i++) {
+      assertEquals(q.peek(), i);
+      assertEquals(q.dequeue(), i);
+    }
+    assertTrue(q.isEmpty());
+    for(int i=0; i<100; i++) {
+      q.enqueue(i);
+      assertFalse(q.isEmpty());
+      assertEquals(q.peek(), 0);
+    }
+    for(int i=0; i<100; i++) {
+      assertEquals(q.peek(), i);
+      assertEquals(q.dequeue(), i);
+    }
+    assertTrue(q.isEmpty());
   }
 
 
